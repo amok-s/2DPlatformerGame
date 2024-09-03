8cec352f1232bd3bf8208218e65deed0fafe1c2c
amok-s <stanek.arkadiusz@gmail.com>
2024-09-03 15:26:55 +0200

    Random-collectables-and-finish-goal
    - there is now more fruits and each time it's a random fruit (randomness can be turned off)
    - instead of finishing line the goal of the levels is to collect all fruits
    - after 10 fruits 1 heart is restored

--- Scripts/collectable.gd
+++ Scripts/collectable.gd
@@ -4,13 +4,22 @@ extends Area2D
 @onready var pick_up = $PickUp
 @onready var collision_shape_2d = $CollisionShape2D
 
+@export_enum("ananas", "apple", "banana", "cherry", "kiwi", "melon", "orange", "strawberry") var fruit: String = "Rebecca"
+@export var random_fruit: bool = true
 
+var fruits: Array = ["ananas", "apple", "banana", "cherry", "kiwi", "melon", "orange", "strawberry"]
 
+func _ready():
+	if random_fruit == true:
+		animated_sprite_2d.animation = fruits.pick_random()
+	else:
+		animated_sprite_2d.animation = fruit
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		animated_sprite_2d.animation = "collected"
 		game_manager.add_point()
+		game_manager.add_fruit()
 		pick_up.play(0)
 		collision_shape_2d.queue_free()
 
--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,19 +2,32 @@ extends Node
 @onready var game_manager = %GameManager
 @onready var points_label = %PointsLabel
 @onready var pick_up = $PickUp
+@onready var fruits_node = $"../Scene Objects/Collectables group"
+
 @export var hearts : Array[Node]
-
-
+@export var next_level : PackedScene
 
 
 var points = 0
 var lives = 3
+var fruits_count = 0
+var fruits_amount
+
+func _ready():
+	fruits_amount = fruits_node.get_child_count()
 
 func add_point():
 	points += 1
 	print(points)
-	points_label.text = "Points: " + str(points)
-	
+
+func add_fruit():
+	fruits_count += 1
+	points_label.text = "Fruits: " + str(points) + " / " + str(fruits_amount)
+	if fruits_count == 10:
+		add_health()
+	if fruits_count == fruits_amount:
+		get_tree().change_scene_to_packed(next_level)
+
 func decrease_health():
 	lives -= 1
 	print(lives)
@@ -26,5 +39,12 @@ func decrease_health():
 	if (lives == 0):
 		await get_tree().create_timer(0.65).timeout
 		get_tree().reload_current_scene()
-	
 
+func add_health():
+	if lives < 3:
+		lives += 1
+	for h in 3:
+		if (h < lives):
+			hearts[h].show()
+		else:
+			hearts[h].hide()
