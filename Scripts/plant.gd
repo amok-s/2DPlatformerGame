9821a7760130b9d1c4ad80c55ddc1c118640d31c
amok-s <stanek.arkadiusz@gmail.com>
2024-09-13 23:33:16 +0200

    Changed and fixedPlant Enemy
    - changed Enemy from RigidBody to CharacterBody
    - fixed issue with timers that was causing app to crash

--- Scripts/plant.gd
+++ Scripts/plant.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var bullet : PackedScene
 @export var sprite : AnimatedSprite2D
@@ -10,60 +10,61 @@ extends RigidBody2D
 @onready var collision_shape_2d = $CollisionShape2D
 @onready var area_2d = $Area2D
 
-var shooting = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var canShoot = true
 
 func timer():
 	var random = randf_range(0, 4)
 	await get_tree().create_timer(random).timeout
-	shooting = true
+	shoot()
+
 
 func _ready():
 	timer()
 	pass 
 
 func _process(_delta):
-	
-	if (shooting == true):
-		shooting = false
-		sprite.animation = "attack"
-		await get_tree().create_timer(0.5).timeout
-		shoot()
-		sprite.animation = "idle"
-		timer()
-
+	move_and_slide()
+	if not is_on_floor():
+		velocity.y += gravity * _delta
+		
 	var playerOnRight = (player.position.x - position.x) > 0
 	sprite.flip_h = playerOnRight
 
 func shoot():
-	
-	if ((player.position.x - position. x) > 0):
-		var b = bullet.instantiate()
-		b.position = position + Vector2(55, -8)
-		b.rotate(PI)
-		get_parent().get_parent().add_child(b)
-	else:
-		var b = bullet.instantiate()
-		b.position = position + Vector2(-55, -8)
-		get_parent().get_parent().add_child(b)
+	if canShoot:
+		sprite.animation = "attack"
+		await get_tree().create_timer(0.5).timeout
+		if ((player.position.x - position. x) > 0):
+			var b = bullet.instantiate()
+			b.position = position + Vector2(55, -8)
+			b.rotate(PI)
+			get_parent().get_parent().add_child(b)
+		else:
+			var b = bullet.instantiate()
+			b.position = position + Vector2(-55, -8)
+			get_parent().get_parent().add_child(b)
 
-	spawn_projectile_sound.play(0)
-
+		spawn_projectile_sound.play(0)
+		sprite.animation = "idle"
+		timer()
 func _on_area_2d_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		var y_delta = position.y - body.position.y
 		var x_delta = body.position.x - position.x
 		print (y_delta)
 		if (y_delta > 30):
+			canShoot = false
 			%GameManager.spawn_blink()
 			%GameManager.spawn_sfx("kill", position + Vector2(0, -25))
 			collision_shape_2d.queue_free()
 			area_2d.queue_free()
-			shooting = false
 			body.velocity.y = -450
 			kill_sound.play(0)
 			sprite.play("hit")
 			var random_x = randf_range(-150, 150)
-			set_axis_velocity(Vector2(random_x, -190))
+			velocity.x = random_x
+			velocity.y = -190
 			await get_tree().create_timer(4).timeout
 			queue_free()
 		else:
@@ -71,3 +72,4 @@ func _on_area_2d_body_entered(body):
 				body.take_damage(-380, -550)
 			else: #approaching from the right
 				body.take_damage(380, -550)
+
