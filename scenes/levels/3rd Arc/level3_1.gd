abeead939a97d8ddf07143911c70fb3ed6ab4202
Arkadiusz 'Amok' Stanek <136911992+amok-s@users.noreply.github.com>
2024-10-21 17:57:38 +0200

    Work on night in level 3-1

---
+++ scenes/levels/3rd Arc/level3_1.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalLevelManager.startLevelMusic($BgMusic)
+	GlobalLevelManager.currentArc = 3
+	GlobalLevelManager.endOfArc = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_bg_music_finished():
+	GlobalLevelManager.music_time = 0
+	GlobalLevelManager.startLevelMusic($BgMusic)
