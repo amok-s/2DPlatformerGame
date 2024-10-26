8bbd0601721f0310e91d7e0f30e36237d3f4fb71
Arkadiusz 'Amok' Stanek <136911992+amok-s@users.noreply.github.com>
2024-10-26 12:34:53 +0200

    Fixed bug with death animation
    - an issue when player sprite will fly away during death animation will no longer appear

--- assets/MainCharacter/scripts/death_state.gd
+++ assets/MainCharacter/scripts/death_state.gd
@@ -4,10 +4,14 @@ class_name PlayerDeath
 @export var player : CharacterBody2D
 @export var sprite : AnimatedSprite2D
 @onready var dead_sound = $"../../DeadSound"
+@onready var collision_shape = $"../../CollisionShape2D"
 
+var death_pos
 
 func Enter():
 	print(self.name)
+	death_pos = player.get_global_position()
+	collision_shape.queue_free()
 	player.velocity.x = 0
 	player.velocity.y = 0
 	player.gravity = 0
@@ -22,7 +26,7 @@ func Enter():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func Update(_delta:float):
-	pass
+	player.global_position = death_pos
 
 func Exit():
 	pass
