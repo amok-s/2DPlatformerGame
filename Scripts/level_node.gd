3ede7c9191299a47af398aed276714fe9bf9bec4
Arkadiusz 'Amok' Stanek <136911992+amok-s@users.noreply.github.com>
2024-11-11 13:20:15 +0100

    Updated into levels
    - updated how intro levels look (chromatc abberation, hid UI etc.)

--- Scripts/level_node.gd
+++ Scripts/level_node.gd
@@ -5,11 +5,14 @@ extends Node
 @export var fruits_left_to_show_sfx : int = 8
 @export var current_arc : int
 @export var end_of_arc : bool = false
+@export var intro : bool = false
 
 func _ready():
 	GlobalLevelManager.startLevelMusic($BgMusic)
 	GlobalLevelManager.currentArc = current_arc
 	GlobalLevelManager.endOfArc = end_of_arc
+	if intro == true:
+		$UI/CoinsPanel.hide()
 
 func _on_bg_music_finished():
 	GlobalLevelManager.music_time = 0
