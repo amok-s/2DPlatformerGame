c77d97375d682c8a43c63cd2000c66ef8bc4e568
Arkadiusz 'Amok' Stanek <136911992+amok-s@users.noreply.github.com>
2024-12-05 01:13:34 +0100

    Level 2-2 finished

---
+++ scenes/levels/2nd Arc/2_2/Camera_02.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+func _ready():
+	connect("body_entered", camEntered)
+	connect("body_exited", camExited)
+
+func camEntered(body):
+	if body.name == "CharacterBody2D":
+		get_parent().cameraChangeZoom(Vector2(1.3, 1.3), 0.8)
+
+func camExited(body):
+	if body.name == "CharacterBody2D":
+		get_parent().cameraChangeZoom(get_parent().originalZoom, 1.2)
---
+++ scenes/levels/2nd Arc/2_2/Camera_03.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _ready():
+	connect("body_entered", camEntered)
+	connect("body_exited", camExited)
+
+func camEntered(body):
+	if body.name == "CharacterBody2D":
+		get_parent().cameraChangeZoom(Vector2(1.2, 1.2), 1.1)
+
+func camExited(body):
+	if body.name == "CharacterBody2D":
+		get_parent().cameraChangeZoom(get_parent().originalZoom, 0.8)
